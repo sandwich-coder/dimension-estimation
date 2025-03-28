61cdc1090ad45f8d93e603b26405ac29b5144fba
brkim <fried4chicken@kentech.ac.kr>
2025-03-28 13:39:02 +0900

    Create experiment.py

---
+++ experiment.py
@@ -0,0 +1,47 @@
+from copy import deepcopy as copy
+import os, sys
+import types
+import logging
+import numpy as np
+import torch
+
+from estimator import DimensionEstimator
+
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler
+from torchvision import datasets as images
+from torchvision.transforms import ToTensor
+
+
+#load
+
+digits = images.MNIST(
+        root = 'datasets',
+        train = True,
+        download = True,
+        transform = ToTensor()
+        ).data.numpy()
+digits = digits.reshape([digits.shape[0], -1])
+
+cloths = images.FashionMNIST(
+        root = 'datasets',
+        train = True,
+        download = True,
+        transform = ToTensor()
+        ).data.numpy()
+cloths = cloths.reshape([cloths.shape[0], -1])
+
+#select
+data = cloths.copy()
+
+
+#processed
+
+data = data.astype('float64')
+
+scaler = MinMaxScaler(feature_range = (-1, 1))
+scaler.fit(data)
+data = scaler.transform(data)
+
+
+estimator = DimensionEstimator()
