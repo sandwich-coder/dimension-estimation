abff79b167eba78b74ea367f0baa52e50b51797d
sandwich-coder <157770762+sandwich-coder@users.noreply.github.com>
2025-04-10 21:03:53 +0900

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -6,8 +6,6 @@ import numpy as np
 from scipy import linalg as la
 from scipy.fft import fftn as ft, ifftn as ift, fftfreq, fftshift
 from scipy.spatial.distance import pdist, cdist
-from scipy.optimize import minimize
-from scipy.stats import median_abs_deviation as mad
 import matplotlib as mpl
 from matplotlib import pyplot as pp
 import torch
