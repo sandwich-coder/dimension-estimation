28a06e62509a97d53ddcb219478e372ec0a55633
sandwich-coder <157770762+sandwich-coder@users.noreply.github.com>
2025-04-12 17:37:33 +0900

    Update mad_align.py

--- practice/mad_align.py
+++ practice/mad_align.py
@@ -55,6 +55,108 @@ def dispersion(X):
     return spread
 
 
+class BareEstimator:
+    def __repr__(self):
+        return 'bare estimator'
+
+    def __call__(
+            self,
+            X,
+            batch_count = 1000,
+            exact = False,
+            divisions = 10
+            ):
+        if type(X) != np.ndarray:
+            raise TypeError('The input must be a \'numpy.ndarray\'.')
+        if X.dtype != np.float64:
+            X = X.astype('float64')
+        if X.ndim != 2:
+            raise ValueError('The shape must be the dataset standard.')
+        if not isinstance(batch_count, int):
+            raise TypeError('\'batch_count\' should be an integer.')
+        if batch_count < 1:
+            raise ValueError('\'batch_count\' must be positive.')
+        if not isinstance(exact, bool):
+            raise TypeError('\'exact\' should be boolean.')
+        if not isinstance(truncate, bool):
+            raise TypeError('\'truncate\' should be boolean.')
+        if not isinstance(divisions, int):
+            raise TypeError('\'divisions\' should be an integer.')
+        if divisions < 2:
+            raise ValueError('\'divisions\' must be greater than 1.')
+        if divisions > 10000:
+            raise ValueError('\'divisions\' greater than 10000 is not supported.')
+        if divisions >= 50:
+            tile_dtype = np.int16
+        else:
+            tile_dtype = np.int8
+
+        # A tile is always adjacent to all the others in the case of two divisions.
+        if divisions == 2:
+            binary = np.where(X >= 0, 1, -1)
+            binary = binary.astype('int8')
+
+            occupied = np.unique(binary, axis = 0)
+            dimension = np.log(occupied.shape[0], dtype = 'float64') / np.log(2, dtype = 'float64')
+            if exact:
+                dimension = dimension.tolist()
+            else:
+                dimension = dimension.round().astype('int64')
+                dimension = dimension.tolist()
+            return dimension
+
+
+        #quantized
+        range_ = np.max(X.max(axis = 0) - X.min(axis = 0), axis = 0)
+        width = range_ / np.float64(divisions)
+        if divisions % 2 != 0:
+            tile = X / width
+        else:
+            tile = X / width - np.float64(0.5)
+        tile = tile.round().astype(tile_dtype)
+        tile = np.unique(tile, axis = 0)
+
+
+        # - counted -
+
+        batch = copy(np.array_split(tile, batch_count, axis = 0))
+        if batch_count > tile.shape[0]:
+            batch = batch[:tile.shape[0]]
+
+        adjacency = []
+        for lll in tqdm(batch, colour = 'magenta'):
+
+            _ = lll.reshape([lll.shape[0], 1, lll.shape[1]])
+            compared = _.repeat(tile.shape[0], axis = 1)
+
+            _ = tile.reshape([tile.shape[0], 1, tile.shape[1]])
+            _ = _.swapaxes(0, 1)
+            all_ = _.repeat(compared.shape[0], axis = 0)
+
+            distance = np.max(
+                    np.absolute(compared - all_),
+                    axis = 2
+                    )
+            is_adjacent = distance == 1
+
+            adjacency_batch = is_adjacent.astype('int64')
+            adjacency_batch = adjacency_batch.sum(axis = 1, dtype = 'int64')
+            adjacency.append(adjacency_batch)
+
+        adjacency = np.concatenate(adjacency, axis = 0)
+
+
+        dimension = np.log(adjacency.mean(axis = 0) + 1, dtype = 'float64') / np.log(3, dtype = 'float64')
+        if exact:
+            dimension = dimension.tolist()
+        else:
+            dimension = dimension.round().astype('int64')
+            dimension = dimension.tolist()
+        return dimension
+
+
+
+
 
 # -- test --
 
